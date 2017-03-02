2dc3645960afaf01fa91409929606135f73b129f
Diky Arga <dikyarga.id@gmail.com>
2017-03-02 12:52:18 +0700

    add login, register and dashboard view

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.json(req.session)
+  // res.json(req.session)
+  console.log(req.session);
+  if(req.session.passport){
+    res.render('dashboard/index', {username: req.session.passport.user})
+
+  } else {
+    res.redirect('/login')
+  }
 })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,56 @@ var router = express.Router();
 var passport = require('passport');
 var Account = require('../models/account');
 
-router.post('/register', function(req, res, next){
-  Account.register(new Account({
-    username : req.body.username
-  }), req.body.password, function(err, account) {
-    console.log('account : ', account);
+router.get('/register', function(req, res, next) {
+    res.render('auth/register')
+})
+
+router.post('/register', function(req, res, next) {
+    Account.register(new Account({
+        username: req.body.username
+    }), req.body.password, function(err, account) {
+        console.log('account : ', account);
         if (err) {
-            return res.render('auth/register', { account : account });
+            return res.render('auth/register', {
+                account: account
+            });
         }
-        passport.authenticate('local')(req, res, function () {
-          res.redirect('/');
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/dashboard');
         });
     });
 })
 
-router.get('/login', function(req, res, next){
-  res.render('auth/login')
+router.get('/login', function(req, res, next) {
+    res.render('auth/login')
 })
 
-router.post('/login', passport.authenticate('local'), function(req, res, next){
-  res.redirect('/dashboard');
+router.post('/login', passport.authenticate('local'), function(req, res, next) {
+    res.redirect('/dashboard');
 })
 
 router.get('/logout', function(req, res) {
-      req.logout();
-      res.redirect('/');
-  });
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            req.logout();
+            res.redirect('/');
+        }
+    })
+});
 
-router.get('/ping', function(req, res, next){
-  res.send("pong!", 200);
+router.get('/ping', function(req, res, next) {
+    res.send("pong!", 200);
 })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(req.session.passport.user)
-  // res.render('index', { title: 'Express', username: req.username });
+    // res.json(req.session.passport.user)
+    res.render('index', {
+        title: 'Express',
+        username: req.username
+    });
 });
 
 module.exports = router;
